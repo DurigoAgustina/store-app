f6127962fe6d1d3d72e45de2af89a385c96f6c8c
Agustina Durigo <agustinadurigo@gmail.com>
2022-11-22 20:32:20 -0300

    Creo el componente CartIcon

---
+++ src/components/commons/CartIcon/CartIcon.jsx
@@ -0,0 +1,10 @@
+import IconCart from '../../../assets/cart-icon.svg'
+import './_CartIcon.scss'
+
+export const CartIcon = () => {
+  return (
+
+    <img src={IconCart} alt='cart icon' className='cart-icon'/>
+
+  )
+}
