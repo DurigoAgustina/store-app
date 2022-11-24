0afa06b44b0ce10a67bfa6daa88e1a1516a93517
Agustina Durigo <agustinadurigo@gmail.com>
2022-11-24 00:22:42 -0300

    Agrego el singleProductSlice al store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productsSlice } from './slices/products/productsSlice';
+import { singleProductSlice } from './slices/singleProduct/singleProductSlice';
 
 export const store = configureStore ({
   reducer: {
-    products: productsSlice.reducer
+    products: productsSlice.reducer,
+    singleProduct: singleProductSlice.reducer
   }
 })
