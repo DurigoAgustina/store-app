2f39ca216d73c26141d1d0e1a83dacdac829a423
Agustina Durigo <agustinadurigo@gmail.com>
2022-11-24 00:23:27 -0300

    Pequeña modificación en thunks de products

--- src/store/slices/products/thunks.js
+++ src/store/slices/products/thunks.js
@@ -4,7 +4,7 @@ import {startLoadingProducts, setProducts} from './productsSlice'
 
 export const getProducts = (page = 0, limit = 12) => {
   return async( dispatch ) => {
-    dispatch( startLoadingProducts )
+    dispatch( startLoadingProducts() )
 
     const {data} = await productsApi.get(`categories/1/products?offset=${page  * 10}&limit=${limit}`)
 
