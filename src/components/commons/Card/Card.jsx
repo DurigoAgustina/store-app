50ddd90cec80564bcc5b40c89b47150b9c969004
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-02 11:55:06 -0300

    Modifico el archivo Card y creo la función handleAddToCart

--- src/components/commons/Card/Card.jsx
+++ src/components/commons/Card/Card.jsx
@@ -1,9 +1,23 @@
 import Button from '../Button/Button'
 import Title from '../Title/Title'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import './_Card.scss'
+import { addToCart } from '../../../store/slices/cart'
+import { productsSlice } from '../../../store/slices/products/productsSlice'
 
 const Card = ({img, title, price, id}) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (e) => {
+    dispatch(addToCart({
+      id,
+      title,
+      img,
+      price,
+    }))
+  }
+
   return (
     <div className='card'>
       <Link to={`/producto/${id}`}>
@@ -12,8 +26,8 @@ const Card = ({img, title, price, id}) => {
       <div className='card__info'>
         <Title Type='h3'>{title}</Title>
         <p className='card__price'>${price}</p>
-          <Button variant='primary'>
-            Agregar al carrito</Button>
+        <Button variant='primary' onClick={() => handleAddToCart(productsSlice)}>
+          Agregar al carrito</Button>
       </div>
     </div>
   )
