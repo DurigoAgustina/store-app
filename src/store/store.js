9d0714f2edaa7ff60efa48069cb9c16b0397cba7
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-02 11:25:25 -0300

    Agrego el cartSlice al store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { cartSlice } from './slices/cart/cartSlice';
 import { productsSlice } from './slices/products/productsSlice';
 import { singleProductSlice } from './slices/singleProduct/singleProductSlice';
 
 export const store = configureStore ({
   reducer: {
     products: productsSlice.reducer,
-    singleProduct: singleProductSlice.reducer
+    singleProduct: singleProductSlice.reducer,
+    cart: cartSlice.reducer,
   }
 })
