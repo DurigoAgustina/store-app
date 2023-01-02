342494d79b8f63e42ad01404a160feb1357fd705
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-02 11:17:36 -0300

    Modifico archivo JSX y SASS de Header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom';
 import { CartIcon } from '../../commons/CartIcon/CartIcon';
 import NavBar from '../../commons/NavBar/NavBar';
+import showCartOffCanvas from '../../../utils/showCartOffCanvas'
 import './_Header.scss';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+  const {quantityCart} = useSelector(state => state.cart)
+
   return (
     <>
       <header className='header'>
@@ -11,7 +16,10 @@ const Header = () => {
           <p className='header__logo'><Link to='/'>StoreApp</Link></p>
           <div className='header__nav'>
             <NavBar />
-            <CartIcon />
+            <button className='header__cart'>
+              <CartIcon onClick={showCartOffCanvas} />
+              <p className='header__cart-quantity'>{quantityCart}</p>
+            </button>
           </div>
         </div>
       </header>
