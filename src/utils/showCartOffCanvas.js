8b5ebb6a53c14163882ed577cbd964761912b3ed
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-02 11:08:45 -0300

    Creo la función showCartOffCanvas

---
+++ src/utils/showCartOffCanvas.js
@@ -0,0 +1,10 @@
+
+const showCartOffCanvas = () => {
+
+  const cartOffCanvas = document.querySelector('.cart-off-canvas')
+
+  cartOffCanvas.classList.toggle('showCartOffCanvas')
+
+}
+
+export default showCartOffCanvas
