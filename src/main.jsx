9984defde86a792a0b59308932965d2fef46c861
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-02 12:20:11 -0300

    Llamo getTotals y getProducts en el Main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.scss'
+import { getTotals } from './store/slices/cart'
+import { getProducts } from './store/slices/products/thunks'
 import { store } from './store/store'
+
+store.dispatch(getTotals());
+store.dispatch(getProducts());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
